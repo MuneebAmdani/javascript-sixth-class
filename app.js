dbc9d94e60ba71e8c39400d48a6ea090f34cc995
MuneebAmdani <arhammuneeb239@gmail.com>
2024-11-14 12:57:03 +0500

    javascript class six

---
+++ app.js
@@ -0,0 +1,72 @@
+// let class1Std=['zubair','muneeb','hanzala'];
+// let class2Std=['umair','sameer','amin'];
+// let stds=[class1Std,class2Std]
+
+// console.log(stds)
+// console.log(stds[0][1],stds[1][2])
+
+// let std1=[]
+// function pushNewStudent(){
+//     let newStdName=document.getElementById("number").value
+// std1.push(newStdName)
+// }
+// console.log(std1)
+
+// const fruits = ["Banana", "Orange", "Lemon", "Samsung", "Mango"];
+// const newFruits = fruits.slice(3);
+// console.log(newFruits)
+
+// const fruits = ["Banana", "Orange", "Lemon", "Samsung", "Mango"];
+// let index_value_of_Samsung=fruits.indexOf("Samsung");
+// const newFruits=fruits.slice(index_value_of_Samsung)
+// console.log(newFruits)
+
+
+// const fruits = ["Banana", 14, "Lemon", "Samsung", "Mango"];
+// let index_value_of_Samsung=fruits.indexOf(14);
+// console.log(index_value_of_Samsung)
+// const newFruits=fruits.slice(index_value_of_Samsung)
+// console.log(newFruits)
+
+// let friends = [["zohaib","azmat"],
+// ["amir","ansari"],
+// [["naveed","mustafa","qasim","sultan"]]
+// ];
+
+// let friendsName=friends[2][0].slice(1,3)
+// console.log(friendsName);
+
+// Create an Array
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// // At position 2, add "Lemon" and "Kiwi"
+// fruits.splice(0, 1, "Lemon","Kiwi");
+// console.log(fruits)
+
+// const newArray = ["Banana", "Orange", "Apple", "Mango",
+//     [
+//         ["Milk","Yougart","Butter"]
+//     ]
+// ];
+// newArray[4][0].splice(1,1,"slice","eggs")
+// console.log(newArray);
+
+// const newArray = ["Banana", "Orange", "Apple", "Mango",
+//     [
+//         ["Milk","Yougart","Butter"]
+//     ]
+// ];
+// let deletedItem=newArray[4][0].splice(1,1,"eggs","slice")
+// console.log(deletedItem)
+// console.log(newArray)
+
+// -----------------------------------for-loop-----------------------------------------
+
+// let std=["zubair","junaid","owais","ayan"]
+// console.log(std.indexOf("owais"))
+
+// for(let i = 0 ; i < 10 ; i++){
+//     console.log("muneeb", i)
+// }
+
+
